c0e2fcf53962278ca0b4a386c7376e54f6d22339
Kopi <ihessmc@gmail.com>
2016-03-16 16:20:24 -0700

    Added basic stuff.

--- src/me/ServerManagement/listener/PlayerListener.java
+++ src/me/ServerManagement/listener/PlayerListener.java
@@ -24,9 +24,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerListener extends JavaPlugin implements Listener {
-
-	 ServerManagement plugin;
-     
+     ServerManagement plugin;
      public PlayerListener (ServerManagement passedPlugin)
      {
      	this.plugin = passedPlugin;
@@ -37,11 +35,16 @@ public class PlayerListener extends JavaPlugin implements Listener {
      public void joinAndOther(PlayerJoinEvent event)
      {
        Player player = event.getPlayer();
-       if (getConfig().getString("owner").contains(player.getName())) 
+       if (getConfig().getStringList("owners").contains(player.getName())) 
        {
          event.setJoinMessage(ChatColor.AQUA + player.getName() + ChatColor.GRAY + " is the " + ChatColor.RED + "Owner" + ChatColor.GRAY + " of the server" + ChatColor.DARK_GRAY + "!");
          player.setOp(true);
-         player.setPlayerListName(ChatColor.RED + player.getName());
+         player.setPlayerListName(ChatColor.RED + "Owner" + ChatColor.WHITE + player.getName());
+       }
+       if (getConfig().getStringList("coowners").contains(player.getName())) {
+         event.setJoinMessage(ChatColor.AQUA + player.getName() + ChatColor.GRAY + " is the " + ChatColor.DARK_AQUA + "Co Owner" + ChatColor.GRAY + " of the server" + ChatColor.DARK_GRAY + "!");
+         player.setOp(true);
+         player.setPlayerListName(ChatColor.DARK_AQUA + "CoOwner" + ChatColor.WHITE + player.getName());       	
        }
      }
      
